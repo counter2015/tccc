ba9ed26924ef9bf303b3dff41165504fc1b2ee72
counter2015 <voidcounter@gmail.com>
2024-04-07 22:19:48 +0800

    format code.

--- src/main/scala/set1/Challenge5.scala
+++ src/main/scala/set1/Challenge5.scala
@@ -1,14 +1,6 @@
 package set1
 
-import set1.Challenge2.toHex
+import misc.Decrypt
 object Challenge5:
   def keyEncrypt(key: String, text: String): String =
-    val times = (text.length + key.length - 1) / key.length
-
-    val bytes =
-      for (a, b) <- text.zip(key.repeat(times).take(text.length))
-      yield (a ^ b).toByte
-
-    val res = bytes.toArray.toHex
-
-    res
+    Decrypt.textByKey(text, key)
